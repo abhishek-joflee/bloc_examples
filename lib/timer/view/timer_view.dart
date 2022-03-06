d1ae122b92ffd9da34b137e1ef2aa3037645545a
iAbhishekPatil <abhishek.patil@joflee.com>
2022-03-06 14:15:34 +0530

    background added

--- lib/timer/view/timer_view.dart
+++ lib/timer/view/timer_view.dart
@@ -11,6 +11,7 @@ class TimerView extends StatelessWidget {
       appBar: AppBar(title: const Text('Flutter Timer')),
       body: Stack(
         children: [
+          const Background(),
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
@@ -95,6 +96,26 @@ class Actions extends StatelessWidget {
           ],
         );
       },
+    );
+  }
+}
+
+class Background extends StatelessWidget {
+  const Background({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Colors.blue.shade50,
+            Colors.blue.shade200,
+            Colors.blue.shade500,
+          ],
+        ),
+      ),
     );
   }
 }
